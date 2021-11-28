17b3b555ef82bb38db8c0048fbeaab9644525605
Gavin Lochtefeld <gavin.loch@gmail.com>
2021-11-28 00:13:25 -0600

    updated URLs

--- minutemindr/urls.py
+++ minutemindr/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('schedule/')),
     path('admin/', admin.site.urls),
-    path('sched/', include('scheduler.urls')),
+    path('schedule/', include('scheduler.urls')),
 ]
